63903a8bd48f112aeb60ae971064192665871826
Erce Kal <erce@futr.ai>
2020-04-06 14:23:36 +0100

    rename

---
+++ chatClient2/.eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  "env": {
+      "browser": true,
+      "es6": true
+  },
+  "parser": "babel-eslint",
+  "extends": ["standard", "standard-react"],
+  "globals": {
+      "Atomics": "readonly",
+      "SharedArrayBuffer": "readonly"
+  },
+  "parserOptions": {
+      "ecmaFeatures": {
+          "jsx": true
+      },
+      "ecmaVersion": 2018,
+      "sourceType": "module"
+  },
+  "plugins": [
+      "react"
+  ],
+  "rules": {
+  }
+};
---
+++ chatClient2/src/App.js
@@ -0,0 +1,45 @@
+import React, { useEffect } from 'react'
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom'
+import { connect } from 'react-redux'
+import * as ChatActions from './store/actions/chatActions'
+
+function App ({ setupSocket }) {
+  useEffect(() => {
+    setupSocket()
+  }, [])
+  return (
+    <div className='App'>
+      <BrowserRouter>
+        <Switch>
+          <Route
+            path='/login'
+            render={props => {
+              return <h1>Login</h1>
+            }}
+          />
+          <Route
+            path='/'
+            render={props => {
+              return <h1>Root</h1>
+            }}
+          />
+        </Switch>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+const mapStateToProps = state => ({
+  ...state.auth,
+  ...state.chat
+})
+
+const mapDispatchToProps = dispatch => ({
+  setupSocket: () => {
+    dispatch(ChatActions.setupSocket())
+  }
+})
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App)
